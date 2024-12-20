a214530984353ff9327c96bf930206a8b5042adf
Fadhila zulfa <24031554093@mhs.unesa.ac.id>
2024-12-20 11:53:15 +0700

    first init github

---
+++ postgres/init.sql
@@ -0,0 +1,7 @@
+-- Initialize custom database schema or tables
+CREATE TABLE IF NOT EXISTS example_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Add any other initialization commands here
